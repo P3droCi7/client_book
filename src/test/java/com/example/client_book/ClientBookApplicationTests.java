d1968febd749f501ceef14c8e523d180f5b1615f
Michal <michalskrzypczak91@gmail.com>
2020-02-16 11:17:25 +0100

    Spriong.io initial commit

---
+++ src/test/java/com/example/client_book/ClientBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.client_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
