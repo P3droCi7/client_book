83e63d257c807eb8792c0c276fb5f53076e536de
Michal <michalskrzypczak91@gmail.com>
2020-03-19 00:50:22 +0100

    Working Backend Application with Frondend View created based on Thymeleaf TODO: Spring Security, Pagination View, Reverse Sorting, Create Inscription View and display all

--- src/main/java/com/example/client_book/driver/web/StartPageController.java
+++ src/main/java/com/example/client_book/driver/web/StartPageController.java
@@ -1,4 +1,20 @@
 package com.example.client_book.driver.web;
 
+import com.example.client_book.driver.repository.InscriptionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class StartPageController {
+
+    @Autowired
+    InscriptionRepository inscriptionRepository;
+
+    @GetMapping("/startpage")
+    private String index(Model model) {
+        model.addAttribute("inscriptionList", inscriptionRepository.findAll());
+        return "index";
+    }
 }
