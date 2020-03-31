cb35c52356aa7abd008f7d132c54df1c85fb0fef
Michal <michalskrzypczak91@gmail.com>
2020-04-01 01:35:06 +0200

    Very important!!!!! CrossOrigin annotation allows to access REST APi from different domain - Angular test aplication

--- src/main/java/com/example/client_book/driver/web/InscriptionController.java
+++ src/main/java/com/example/client_book/driver/web/InscriptionController.java
@@ -10,10 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -34,6 +31,7 @@ public class InscriptionController {
         return responseEntity;
     }
 
+    @CrossOrigin
     @GetMapping("/showall")
     private ResponseEntity<List<Inscription>> showAllInscriptions() {
         List<Inscription> inscriptionList = inscriptionService.returnAllInscriptions();
